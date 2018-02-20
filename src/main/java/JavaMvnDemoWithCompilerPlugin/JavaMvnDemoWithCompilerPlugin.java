84bbc218f964ee31c6979ec61521513b1346755b
ag90 <36640133+ag90@users.noreply.github.com>
2018-02-20 20:26:04 +0530

    hello UN

--- src/main/java/JavaMvnDemoWithCompilerPlugin/JavaMvnDemoWithCompilerPlugin.java
+++ src/main/java/JavaMvnDemoWithCompilerPlugin/JavaMvnDemoWithCompilerPlugin.java
@@ -4,7 +4,7 @@ public class JavaMvnDemoWithCompilerPlugin {
 	
 	public  static void method()
 	{
-		System.out.println("Hello All over World");
+		System.out.println("Hello UN");
 	}
 
 	public static void main(String[] args) {
